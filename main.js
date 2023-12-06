f7fc168c2eb155dff565162843c0e44a8c415319
Miguel Peixoto Portela Bispo <miguelportelabispo@gmail.com>
2023-12-06 12:47:22 -0300

    .env

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const booksRoute = require("./routes/books");
 const categoriesRoute = require("./routes/categories");
+const port = process.env.PORT||5000;
 
 app.use(express.json());
 app.use("/books", booksRoute);
 app.use("/categories", categoriesRoute);
 app.use("*", express.static("public"));
 
-mongoose.connect("mongodb://0.0.0.0/bookstore");
+mongoose.connect(process.env.ATLAS_URI);
 
-app.listen(5000)
+app.listen(port);
